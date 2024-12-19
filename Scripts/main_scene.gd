5270e1fbb3a46b0809a1cd2c81082c4f0f87a922
uluj <gungoruluc@gmail.com>
2024-12-19 17:17:16 +0300

    commit

--- Scripts/main_scene.gd
+++ Scripts/main_scene.gd
@@ -3,9 +3,13 @@ extends Node2D
 # Reference to the health bar (adjusted to the correct path)
 @onready var health_bar = $"/root/MainScene/Control/HealthBar"  # Adjusted the path to HealthBar node
 
+# References to the scene prefabs
+var idCard_scene: PackedScene = preload("res://Scenes/idCard.tscn")
+var document_scene: PackedScene = preload("res://Scenes/document.tscn")
+
 # Spawn olan nesneyi tutacak değişkenler
-var spawned_object1 = null
-var spawned_object2 = null
+var spawned_id_card = null
+var spawned_document = null
 
 # Spawn butonu tetiklenince
 func _on_button_pressed():
@@ -13,14 +17,22 @@ func _on_button_pressed():
 	var is_it_true1 = randi_range(0, 1) == 1  # Random true or false
 	var is_it_true2 = randi_range(0, 1) == 1  # Random true or false
 	
-	# Compare the two bools and print success/fail message
-	if is_it_true1 == is_it_true2:
-		print("Belge 1: %s, Belge 2: %s, Outcome: Success" % [is_it_true1, is_it_true2])
-		# No need to remove a health point on success
+	# Instantiate and spawn the idCard and document scenes
+	spawned_id_card = spawn_scene(idCard_scene, Vector2(1044, 718))  # Position for idCard
+	spawned_document = spawn_scene(document_scene, Vector2(844, 718))  # Position for document
+
+
+
+# Function to spawn a scene at a specific position
+func spawn_scene(scene: PackedScene, position: Vector2) -> Node:
+	if scene:
+		var instance = scene.instantiate()
+		add_child(instance)
+		instance.position = position
+		return instance
 	else:
-		print("Belge 1: %s, Belge 2: %s, Outcome: Fail" % [is_it_true1, is_it_true2])
-		# Call the method to update the health bar on failure
-		health_bar.update_health_bar(false)
+		print("Error: Scene is not set properly.")
+		return null
 
 # Kabul alanına girildiğinde
 func _on_kabul_alani_body_entered(body):
@@ -29,8 +41,8 @@ func _on_kabul_alani_body_entered(body):
 		print("Belge: %s, Kabul Edildi, Outcome: Success" % [is_it_true])
 		InteractionController.any_object_dragging = false
 		
-		# Handle success, no objects to remove
-		body.queue_free() # Remove the current object from the scene
+		# Handle success, remove the current object from the scene
+		body.queue_free() 
 
 # Reddetme alanına girildiğinde
 func _on_red_alani_body_entered(body):
@@ -39,5 +51,5 @@ func _on_red_alani_body_entered(body):
 		print("Belge: %s, Reddedildi, Outcome: Fail" % [is_it_true])
 		InteractionController.any_object_dragging = false
 		
-		# Handle failure, no objects to remove
-		body.queue_free() # Remove the current object from the scene
+		# Handle failure, remove the current object from the scene
+		body.queue_free()
